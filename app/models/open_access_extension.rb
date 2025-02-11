1c89a58c7d3beb08274ca3f7c88e489735a7b54a
sanotaras <t.zhyhulin@sanoscience.org>
2025-02-11 21:40:05 +0100

    open access extension model fix

--- app/models/open_access_extension.rb
+++ app/models/open_access_extension.rb
@@ -13,7 +13,7 @@ class OpenAccessExtension < ApplicationRecord
     private
 
     def validate_gold_fields
-        if gold?
+        if green?
             errors.add(:gold_oa_charges, "should be empty for green OA") if gold_oa_charges.present?
             errors.add(:gold_oa_funding_source, "should be empty for green OA") if gold_oa_funding_source.present?
         end

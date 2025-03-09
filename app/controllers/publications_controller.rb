b0b43b601faa31b530c2d2acfeb50727f15aac72
sanotaras <t.zhyhulin@sanoscience.org>
2025-03-09 21:00:30 +0100

    rubocop

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -12,7 +12,7 @@ class PublicationsController < ApplicationController
 
     if publication_params[:conference_attributes][:name]&.present?
       conference = Conference.new(publication_params[:conference_attributes])
-      
+
       if conference.save
         @publication.conference = conference
       else
@@ -29,7 +29,7 @@ class PublicationsController < ApplicationController
 
     if publication_params[:journal_issue_attributes][:title]&.present?
       journal_issue = JournalIssue.new(publication_params[:journal_issue_attributes])
-      
+
       if journal_issue.save
         @publication.journal_issue = journal_issue
       else
@@ -45,7 +45,7 @@ class PublicationsController < ApplicationController
     end
 
     if @publication.save
-      redirect_to @publication, notice: 'Publication was successfully created.'
+      redirect_to @publication, notice: "Publication was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -75,7 +75,7 @@ class PublicationsController < ApplicationController
     end
 
     if @publication.update(publication_params)
-      redirect_to @publication, notice: 'Publication was successfully updated.'
+      redirect_to @publication, notice: "Publication was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
@@ -84,7 +84,7 @@ class PublicationsController < ApplicationController
   def destroy
     @publication = Publication.find(params[:id])
     @publication.destroy
-    
+
     redirect_to publications_path, notice: "Publication was successfully deleted."
   end
 
@@ -94,13 +94,13 @@ class PublicationsController < ApplicationController
     params.require(:publication).permit(
       :title, :category, :status, :author_list, :publication_date, :link,
       :conference_id, :journal_issue_id,
-      research_group_publications_attributes: [:id, :research_group, :is_primary, :_destroy],
-      identifiers_attributes: [:id, :category, :value, :_destroy],
-      repository_links_attributes: [:id, :repository, :value, :_destroy],
-      kpi_reporting_extension_attributes: [:id, :teaming_reporting_period, :invoice_number, :pbn, :jcr, :is_added_ft_portal, :is_checked, :is_new_method_technique, :is_methodology_application, :is_polish_med_researcher_involved, :subsidy_points, :_destroy],
-      open_access_extension_attributes: [:id, :category, :gold_oa_charges, :gold_oa_funding_source, :_destroy],
-      conference_attributes: [:id, :name, :core, :start_date, :end_date, :_destroy],
-      journal_issue_attributes: [:id, :title, :journal_num, :publisher, :volume, :impact_factor, :_destroy]
+      research_group_publications_attributes: [ :id, :research_group, :is_primary, :_destroy ],
+      identifiers_attributes: [ :id, :category, :value, :_destroy ],
+      repository_links_attributes: [ :id, :repository, :value, :_destroy ],
+      kpi_reporting_extension_attributes: [ :id, :teaming_reporting_period, :invoice_number, :pbn, :jcr, :is_added_ft_portal, :is_checked, :is_new_method_technique, :is_methodology_application, :is_polish_med_researcher_involved, :subsidy_points, :_destroy ],
+      open_access_extension_attributes: [ :id, :category, :gold_oa_charges, :gold_oa_funding_source, :_destroy ],
+      conference_attributes: [ :id, :name, :core, :start_date, :end_date, :_destroy ],
+      journal_issue_attributes: [ :id, :title, :journal_num, :publisher, :volume, :impact_factor, :_destroy ]
     )
   end
 end

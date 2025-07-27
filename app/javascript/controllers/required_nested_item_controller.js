23725f2847d3312263fc55b66e82bd13d51466b6
sanotaras <t.zhyhulin@sanoscience.org>
2025-07-28 01:00:30 +0200

    fixed required nested item verification + enforced one primary

--- app/javascript/controllers/required_nested_item_controller.js
+++ app/javascript/controllers/required_nested_item_controller.js
@@ -5,35 +5,51 @@ export default class extends Controller {
   connect() {
     const $container = $(this.element)
 
-    const $nestedFields = $container.find('.nested-fields')
+    const $nestedFields = $container.find('.nested-fields:visible')
 
     $container.off('cocoon:after-insert')
     $container.off('cocoon:after-remove')
 
     $container.on('cocoon:after-insert', () => {
-      let $newNestedFields = $container.find('.nested-fields')
+      let $newNestedFields = $container.find('.nested-fields:visible')
       if ($newNestedFields.length > 1) {
         for (let i = 0; i < $newNestedFields.length; i++) {
           const $nestedField = $newNestedFields.eq(i)
           const $removeLink = $nestedField.find('.remove_fields')
           $removeLink.show()
         }
+        const $lastNestedField = $newNestedFields.last()
+        const $primaryCheckbox = $lastNestedField.find('.primary-checkbox')
+        const $primaryField = $lastNestedField.find('.primary-field')
+        $primaryCheckbox.prop('checked', false)
+        $primaryField.val(false)
       }
     })
 
     $container.on('cocoon:after-remove', () => {
-      let $newNestedFields = $container.find('.nested-fields')
+      let $newNestedFields = $container.find('.nested-fields:visible')
       if ($newNestedFields.length === 1) {
         const $nestedField = $newNestedFields.first()
         const $removeLink = $nestedField.find('.remove_fields')
         $removeLink.hide()
       }
+      const $firstNestedField = $newNestedFields.first()
+      const $primaryCheckbox = $firstNestedField.find('.primary-checkbox')
+      const $primaryField = $firstNestedField.find('.primary-field')
+
+      $primaryCheckbox.prop('checked', true)
+      $primaryField.val(true)
     })
 
     if ($nestedFields.length === 1) {
       const $nestedField = $nestedFields.first()
       const $removeLink = $nestedField.find('.remove_fields')
+      const $primaryCheckbox = $nestedField.find('.primary-checkbox')
+      const $primaryField = $nestedField.find('.primary-field')
+
       $removeLink.hide()
+      $primaryCheckbox.prop('checked', true)
+      $primaryField.val(true)
     }
   }
 }
